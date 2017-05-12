bda3bc6ac740e705db67f2ac1fa7238f1ff6a527
Vasmal <malaivasili@gmail.com>
2017-05-12 23:35:19 +0300

    Интерактивная карта (разметка)

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
-	
-	var button = document.querySelector(".button-feedback");
-	var popup = document.querySelector(".feedback");
-	var overlay = document.querySelector(".modal-overlay");
-	var close = popup.querySelector(".feedback-close");
+var button = document.querySelector(".button-feedback");
+var popup = document.querySelector(".feedback");
+var overlay = document.querySelector(".modal-overlay");
+var close = popup.querySelector(".feedback-close");
 
-	button.addEventListener("click", function(event) {
-		event.preventDefault();
-		popup.classList.add("popup-show");
-		overlay.classList.add("popup-show");
-      });
+button.addEventListener("click", function(event) {
+	event.preventDefault();
+	popup.classList.add("popup-show");
+	overlay.classList.add("popup-show");
+});
 
-      close.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.remove("popup-show");
-        overlay.classList.remove("popup-show");
-      });
+close.addEventListener("click", function(event) {
+	event.preventDefault();
+  	popup.classList.remove("popup-show");
+  	overlay.classList.remove("popup-show");
+});
