272abe84404a056b1e8faaf58bc285f6434ddb5f
Vasmal <malaivasili@gmail.com>
2017-05-12 23:50:16 +0300

    Скрипт карты

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,19 @@ close.addEventListener("click", function(event) {
   	popup.classList.remove("popup-show");
   	overlay.classList.remove("popup-show");
 });
+
+ymaps.ready(function () {
+	var myMap = new ymaps.Map('yandex-map', {
+  		center: [59.939621, 30.330832],
+  		zoom: 15
+	});
+
+	var myMarker = new ymaps.Placemark([59.938615, 30.322947], {}, {
+  		iconLayout: 'default#image',
+  		iconImageHref: '../img/map-marker.png',
+  		iconImageSize: [218, 142],
+  		iconImageOffset: [-109, -142]
+	});
+
+	myMap.geoObjects.add(myMarker);
+});
