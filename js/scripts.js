f79af05e8be8b97bcca5b069b8ffda436189ea32
Vasmal <malaivasili@gmail.com>
2017-05-15 13:14:52 +0300

    Добавил условия в скрипт

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,36 @@ var popup = document.querySelector(".feedback");
 var overlay = document.querySelector(".modal-overlay");
 var close = document.querySelector(".feedback-close");
 
-button.addEventListener("click", function(event) {
-	event.preventDefault();
-	popup.classList.add("popup-show");
-	overlay.classList.add("popup-show");
-});
-
-close.addEventListener("click", function(event) {
-	event.preventDefault();
-  	popup.classList.remove("popup-show");
-  	overlay.classList.remove("popup-show");
-});
-
-ymaps.ready(function () {
-	var myMap = new ymaps.Map('yandex-map', {
-  		center: [59.939621, 30.330832],
-  		zoom: 15
+if (button) {
+	button.addEventListener("click", function(event) {
+		event.preventDefault();
+		popup.classList.add("popup-show");
+		overlay.classList.add("popup-show");
 	});
+}
 
-	var myMarker = new ymaps.Placemark([59.938615, 30.322947], {}, {
-  		iconLayout: 'default#image',
-  		iconImageHref: 'img/map-marker.png',
-  		iconImageSize: [218, 142],
-  		iconImageOffset: [-40, -142]
+if (close) {
+	close.addEventListener("click", function(event) {
+		event.preventDefault();
+	  	popup.classList.remove("popup-show");
+	  	overlay.classList.remove("popup-show");
 	});
+}
 
-	myMap.geoObjects.add(myMarker);
-});
+if (ymaps) {
+	ymaps.ready(function () {
+		var myMap = new ymaps.Map('yandex-map', {
+	  		center: [59.939621, 30.330832],
+	  		zoom: 15
+		});
+
+		var myMarker = new ymaps.Placemark([59.938615, 30.322947], {}, {
+	  		iconLayout: 'default#image',
+	  		iconImageHref: 'img/map-marker.png',
+	  		iconImageSize: [218, 142],
+	  		iconImageOffset: [-40, -142]
+		});
+
+		myMap.geoObjects.add(myMarker);
+	});
+}
